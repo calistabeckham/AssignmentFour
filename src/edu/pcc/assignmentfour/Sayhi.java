7094512ce1a31da6b521c30a70ea7ad0254d1c56
Calista Beckham <calista.beckham@pcc.edu>
2018-02-02 20:25:38 -0800

    printdate statement added

--- src/edu/pcc/assignmentfour/Sayhi.java
+++ src/edu/pcc/assignmentfour/Sayhi.java
@@ -1,5 +1,7 @@
 package edu.pcc.assignmentfour;
 
+import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.date;
+
 /**
  * Created by Calista.Beckham on 2/2/2018
  */
@@ -7,5 +9,6 @@ public class Sayhi {
     public static void main(String[] args){
         System.out.println("Hello");
         System.out.println("My name is Bob!");
+        System.out.println("Toay's date is " + date());
     }
 }
