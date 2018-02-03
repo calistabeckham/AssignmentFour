e95a25f8a98ba389a8ab02a2a52169149b70a797
Calista Beckham <calista.beckham@pcc.edu>
2018-02-02 20:21:29 -0800

    Add statement

--- src/edu/pcc/assignmentfour/Sayhi.java
+++ src/edu/pcc/assignmentfour/Sayhi.java
@@ -1,5 +1,11 @@
 package edu.pcc.assignmentfour;
 
+/**
+ * Created by Calista.Beckham on 2/2/2018
+ */
 public class Sayhi {
-
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("My name is Bob!");
+    }
 }
